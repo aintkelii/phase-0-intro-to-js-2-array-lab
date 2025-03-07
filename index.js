5e6a2200c3cfa5a8b058b0d6100d8b142f618adf
george keli <keligeorge558@gmail.com>
2025-03-07 18:24:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+function appendCat(cat) {
+  return [...cats, cat];
+} 
+function destructivelyAppendCat(cat) {
+  cats.push(cat);
+}
+function prependCat(cat) {
+  return [cat, ...cats];
+}
+function destructivelyPrependCat(cat) {
+  cats.unshift(cat);
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+module.exports = cats;
